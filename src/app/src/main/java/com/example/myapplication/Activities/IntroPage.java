dbb1c959fe377177f4395b582c60f689b27955c9
Wen <u7706423@anu.edu.au>
2024-04-20 18:01:17 +1000

    add IntroPage

---
+++ src/app/src/main/java/com/example/myapplication/Activities/IntroPage.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+public class IntroPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_page);
+
+        setButton();
+    }
+
+    private void setButton(){
+
+        TextView btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroPage.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroPage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+}
