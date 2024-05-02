7e43c52e6c14967e2e7c50f2d0fa514b22b5b9df
Wanzhong Wu <u7642453@anu.edu.au>
2024-05-02 17:07:45 +1000

    Design the Token, determine the key word

--- src/app/src/main/java/com/example/myapplication/basicClass/Token.java
+++ src/app/src/main/java/com/example/myapplication/basicClass/Token.java
@@ -4,13 +4,13 @@ import java.lang.reflect.Type;
 
 public class Token {
 
-    public enum Type{LOCATION, CONDITION, PRICE, Category};
+    public enum Type{LOCATION, Category, NAME, AMOUNT};
 
     private final String token;
     private final Type type;
 
     public Token(String token, Type type) {
-        this.token = token;
+        this.token = token.toLowerCase();
         this.type = type;
     }
 
--- src/app/src/main/java/com/example/myapplication/basicClass/Tokenizer.java
+++ src/app/src/main/java/com/example/myapplication/basicClass/Tokenizer.java
@@ -3,7 +3,8 @@ package com.example.myapplication.basicClass;
 public class Tokenizer {
     private String buffer;
     private Token currentToken;
-
+    private final String categoryCheck = "electronic, clothing, furniture, books, sports, toys, beauty";
+    private final String locationCheck = "sydney, melbourne, queensland, adelaide, tasmania, canberra, south australia";
 
     public Tokenizer(String text) {
         buffer = text;
@@ -12,9 +13,52 @@ public class Tokenizer {
 
 
     public void next(){
-        /*
-        TODO: We haven't know what does the data looks like, we will implement this later.
-         */
+        buffer = buffer.trim();
+
+        if (buffer.isEmpty()) {
+            currentToken = null;
+            return;
+        }
+        String input = "";
+        boolean keepChecking = true;
+        int n = 0;
+
+        while (keepChecking && n < buffer.length()) {
+            char check = buffer.charAt(n);
+            if (check != ' ') {
+                input += check;
+                n++;
+            }
+            else {
+                keepChecking = false;
+            }
+        }
+        if (categoryCheck.contains(input)) {
+            currentToken = new Token(input, Token.Type.Category);
+        }
+        else if (locationCheck.contains(input)) {
+            currentToken = new Token(input, Token.Type.LOCATION);
+        }
+        else if (input.equals("a")) {
+            currentToken = new Token(input, Token.Type.AMOUNT);
+        }
+        else {
+            currentToken = new Token(input, Token.Type.NAME);
+        }
+
+        int tokenLen = currentToken.getToken().length();
+        buffer = buffer.substring(tokenLen);
     }
 
+    public Token current() {
+        {
+            return currentToken;
+        }
+    }
+
+    public boolean hasNext() {
+        {
+            return currentToken != null;
+        }
+    }
 }
