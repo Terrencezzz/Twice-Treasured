435830d54dbb9a2552e65cdd1c33f2c1b26d1800
Wanzhong Wu <u7642453@anu.edu.au>
2024-05-02 21:06:14 +1000

    Implement the abstract class EXP

--- src/app/src/main/java/com/example/myapplication/basicClass/EXP.java
+++ src/app/src/main/java/com/example/myapplication/basicClass/EXP.java
@@ -1,12 +1,32 @@
 package com.example.myapplication.basicClass;
 
+import androidx.annotation.NonNull;
+
+import com.example.myapplication.common.AVLTree;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public abstract class EXP {
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    public DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    public void  fetchProductsAndBuildTree() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                AVLTree<Product> productAVLTree = new AVLTree<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Product product = snapshot.getValue(Product.class);
+                    productAVLTree.insert(product);
+                }
+            }
 
-
-
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("The read failed: " + error.getCode());
+            }
+        });
+    }
 
 }
